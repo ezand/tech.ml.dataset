ae804db97d2f787b8b58140abe0fb596bb3672bd
Eirik Stenersen Sand <eirik.sand@ardoq.com>
2023-10-25 08:53:43 +0200

    Implement methods to access the stable sheet id

--- java/tech/v3/dataset/Spreadsheet.java
+++ java/tech/v3/dataset/Spreadsheet.java
@@ -12,6 +12,8 @@ public class Spreadsheet
   public interface Sheet extends Iterable
   {
     public String name();
+    public String id();
+    public String stableId();
   }
   public interface Row extends Iterable
   {
